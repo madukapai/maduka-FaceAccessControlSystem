3aa764b2c403c5b384ffe18186c4ca8133c457a5
maduka <maduka@MADUKA-NB>
2018-04-29 00:09:18 +0800

    no message

--- FaceAccessMobile/www/scripts/config.js
+++ FaceAccessMobile/www/scripts/config.js
@@ -1,8 +1,8 @@
-﻿var FaceApiRootUrl = "https://westus.api.cognitive.microsoft.com/face/v1.0/";
-var FaceApiKey = "b3f9757983064d5490aa9a90a389d66e";
+﻿var FaceApiRootUrl = "[在這裡放上FaceApi根目錄]";
+var FaceApiKey = "[FaceApi金鑰]";
 
-var CompoterVisionApiRootUrl = "https://westus.api.cognitive.microsoft.com/vision/v1.0/";
-var CompoterVisionApiKey = "8c55cdc528884cd48e4228b42510e90c";
+var CompoterVisionApiRootUrl = "[在這裡放上ComputerVisionApi根目錄]";
+var CompoterVisionApiKey = "[ComputerVisionApi金鑰]";
 
 var TranslaterUrl = "https://api.microsofttranslator.com/V2/Http.svc/Translate";
-var TranslaterApiKey = "abeb23ed7fea4840a257de1076970a77";
+var TranslaterApiKey = "[TextTranslatorApi金鑰]";
